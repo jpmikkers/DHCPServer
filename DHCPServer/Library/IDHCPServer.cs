cf69942d0f288df67afdc516cebd24d5c8911970
jpmikkers_cp <SND\jpmikkers_cp>
2013-12-08 16:20:25 +0000

    IP reservation support

--- DHCPServer/Library/IDHCPServer.cs
+++ DHCPServer/Library/IDHCPServer.cs
@@ -24,6 +24,9 @@ THE SOFTWARE.
 using System;
 using System.Net;
 using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace CodePlex.JPMikkers.DHCP
 {
@@ -57,6 +60,91 @@ namespace CodePlex.JPMikkers.DHCP
         }
     }
 
+    public class ReservationItem
+    {
+        private static readonly Regex regex = new Regex(@"^(?<mac>([0-9a-fA-F][0-9a-fA-F][:\-\.]?)+)(?<netmask>/[0-9]+)?", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+        private string m_MacTaste;
+        private byte[] m_Prefix;
+        private int m_PrefixBits;
+
+        public string MacTaste 
+        {
+            get { return m_MacTaste; }
+
+            set
+            {
+                m_MacTaste = value;
+                m_Prefix = null;
+                m_PrefixBits = 0;
+
+                if (!string.IsNullOrWhiteSpace(MacTaste))
+                {
+                    try
+                    {
+                        Match match = regex.Match(m_MacTaste);
+                        if (match.Success && match.Groups["mac"].Success)
+                        {
+                            m_Prefix = Utils.HexStringToBytes(match.Groups["mac"].Value);
+                            m_PrefixBits = m_Prefix.Length * 8;
+
+                            if (match.Groups["netmask"].Success)
+                            {
+                                m_PrefixBits = Int32.Parse(match.Groups["netmask"].Value.Substring(1));
+                            }
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+
+        public string HostName { get; set; }
+        public IPAddress PoolStart { get; set; }
+        public IPAddress PoolEnd { get; set; }
+
+        private static bool MacMatch(byte[] mac, byte[] prefix, int bits)
+        {
+            // prefix should have more bits than masklength
+            if (((bits + 7) >> 3) > prefix.Length) return false;
+            // prefix should be shorter or equal to mac address
+            if (prefix.Length > mac.Length) return false;
+            for (int t = 0; t < (bits - 7); t += 8)
+            {
+                if (mac[t >> 3] != prefix[t >> 3]) return false;
+            }
+
+            if ((bits & 7) > 0)
+            {
+                byte bitMask = (byte)(0xFF00 >> (bits & 7));
+                if ((mac[bits >> 3] & bitMask) != (prefix[bits >> 3] & bitMask)) return false;
+            }
+            return true;
+        }
+
+        public bool Match(DHCPMessage message)
+        {
+            var client = DHCPClient.CreateFromMessage(message);
+
+            if (!string.IsNullOrWhiteSpace(MacTaste) && m_Prefix!=null)
+            {
+                return MacMatch(client.HardwareAddress, m_Prefix, m_PrefixBits);
+            }
+            else if (!string.IsNullOrWhiteSpace(HostName))
+            {
+                if (!string.IsNullOrWhiteSpace(client.HostName))
+                {
+                    if (client.HostName.StartsWith(HostName,true,CultureInfo.InvariantCulture))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
     public interface IDHCPServer : IDisposable
     {
         event EventHandler<DHCPTraceEventArgs> OnTrace;
@@ -73,6 +161,7 @@ namespace CodePlex.JPMikkers.DHCP
         string HostName { get; }
         bool Active { get; }
         List<OptionItem> Options { get; set; }
+        List<ReservationItem> Reservations { get; set; }
 
         void Start();
         void Stop();
