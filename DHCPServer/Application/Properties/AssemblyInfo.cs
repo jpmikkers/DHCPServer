f69a1e2a452476b0a8c61dc3a06357a64123f308
jpmikkers <jean.paul.mikkers@gmail.com>
2020-07-26 15:36:14 +0200

    Fix some copyright strings

--- DHCPServer/Application/Properties/AssemblyInfo.cs
+++ DHCPServer/Application/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("DHCPServer")]
 [assembly: AssemblyDescription(
-    "Copyright (c) 2010-2019 Jean-Paul Mikkers"+
+    "Copyright (c) 2010-2020 Jean-Paul Mikkers"+
     "\r\n" +
     "\r\n" +
     "Permission is hereby granted, free of charge, to any person obtaining a copy of this software"+
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("JPMikkers")]
 [assembly: AssemblyProduct("DHCPServer")]
-[assembly: AssemblyCopyright("Copyright © Jean-Paul Mikkers 2010-2019")]
+[assembly: AssemblyCopyright("Copyright © Jean-Paul Mikkers 2010-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
