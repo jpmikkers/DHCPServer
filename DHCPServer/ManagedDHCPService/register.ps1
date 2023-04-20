f45c130b2f696d3047facc2038118f2f878f4b0d
jpmikkers <jean.paul.mikkers@gmail.com>
2023-04-20 13:59:06 +0200

    Fancier registration script with firewall creation

--- DHCPServer/ManagedDHCPService/register.ps1
+++ DHCPServer/ManagedDHCPService/register.ps1
@@ -1,12 +1,34 @@
+#Requires -RunAsAdministrator
+
 # sc create ManagedDHCPServer binPath="C:\Users\mikmak\Desktop\tmptmp\ManagedDHCPServer\bin\Release\net7.0-windows\win-x64\publish\ManagedDHCPServer.exe"
 $serviceExe=get-item ".\ManagedDHCPService.exe"
 
-$service=Get-Service -Name 'ManagedDHCPServer'
-if($service -ne $null)
+if($null -eq $serviceExe)
 {
-	"Service already registered"
+	throw "Can't find ManagedDHCPService.exe"
+}
+
+$service=Get-Service -Name 'ManagedDHCPServer' -ErrorAction SilentlyContinue
+if($null -eq $service)
+{
+	# scrub stale firewall rules..
+	Get-NetFirewallRule -Name 'ManagedDHCPServerIn' -ErrorAction SilentlyContinue | Remove-NetFireWallRule
+	Get-NetFirewallRule -Name 'ManagedDHCPServerOut' -ErrorAction SilentlyContinue | Remove-NetFirewallRule
+
+	"Creating firewall rules.."
+
+	# create new firewall rules
+	New-NetFirewallRule -Name 'ManagedDHCPServerIn' -DisplayName 'Managed DHCP Server (inbound)' -Direction Inbound -Action Allow -Protocol UDP -LocalPort 67 -Program $serviceExe.FullName | Out-Null
+	New-NetFirewallRule -Name 'ManagedDHCPServerOut' -DisplayName 'Managed DHCP Server (outbound)' -Direction Outbound -Action Allow -Protocol UDP -LocalPort 67 -Program $serviceExe.FullName | Out-Null
+
+	"Registering service .."
+
+	# register service
+	New-Service -Name 'ManagedDHCPServer' -DisplayName 'Managed DHCP Server' -Description "Managed DHCP Server: $($serviceExe.FullName)" -BinaryPathName $serviceExe.FullName -StartupType Manual -DependsOn ('tcpip') | Out-Null
 }
 else
 {
-	New-Service -Name 'ManagedDHCPServer' -DisplayName 'Managed DHCP Server' -BinaryPathName $serviceExe.FullName -StartupType Manual
+	"Service already registered"
 }
+
+"Done."
--- DHCPServer/ManagedDHCPService/unregister.ps1
+++ DHCPServer/ManagedDHCPService/unregister.ps1
@@ -1,8 +1,18 @@
+#Requires -RunAsAdministrator
+
 # sc delete ManagedDHCPServer
 
-$service=Get-Service -Name 'ManagedDHCPServer'
-if($service -ne $null)
+$service=Get-Service -Name 'ManagedDHCPServer' -ErrorAction SilentlyContinue
+if($null -ne $service)
 {
+	"Stopping service.."
 	$service | Stop-Service
+	"Removing service.."
 	$service | Remove-Service
 }
+
+"Removing firewall rules.."
+Get-NetFirewallRule -Name 'ManagedDHCPServerIn' -ErrorAction SilentlyContinue | Remove-NetFireWallRule
+Get-NetFirewallRule -Name 'ManagedDHCPServerOut' -ErrorAction SilentlyContinue | Remove-NetFirewallRule
+
+"Done."
