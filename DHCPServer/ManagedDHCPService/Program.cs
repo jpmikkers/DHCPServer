0adffe8d30caf3c0a4038a06d0a0ee874c8a3645
jpmikkers <jean.paul.mikkers@gmail.com>
2024-02-07 17:09:18 +0100

    create linux specific main that can be used by systemd

--- DHCPServer/ManagedDHCPService/Program.cs
+++ DHCPServer/ManagedDHCPService/Program.cs
@@ -2,39 +2,51 @@ using ManagedDHCPService;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
 
-HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddWindowsService(options =>
+if(OperatingSystem.IsWindows())
 {
-    options.ServiceName = "Managed DHCP Service";
-});
+    // see https://consultwithgriff.com/building-window-services-in-dotnet/
+    var builder = Host.CreateApplicationBuilder(args);
 
-LoggerProviderOptions.RegisterProviderOptions<
-    EventLogSettings, EventLogLoggerProvider>(builder.Services);
-
-builder.Services.AddSingleton<Worker>();
-builder.Services.AddHostedService<Worker>();
-
-// See: https://github.com/dotnet/runtime/issues/47303
-builder.Logging.AddConfiguration(
-    builder.Configuration.GetSection("Logging"));
-
-IHost host = builder.Build();
-host.Run();
-
-
-/*
-// see https://consultwithgriff.com/building-window-services-in-dotnet/
-
-IHost host = Host.CreateDefaultBuilder(args)
-    .UseWindowsService(config =>
+    builder.Services.AddWindowsService(options =>
     {
-        config.ServiceName = "Managed DHCP Server Service";
-    })
+        options.ServiceName = "Managed DHCP Service";
+    });
+
+    LoggerProviderOptions.RegisterProviderOptions<
+        EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+    builder.Services.AddSingleton<Worker>();
+    builder.Services.AddHostedService<Worker>();
+
+    // See: https://github.com/dotnet/runtime/issues/47303
+    builder.Logging.AddConfiguration(
+        builder.Configuration.GetSection("Logging"));
+
+    var host = builder.Build();
+    host.Run();
+}
+else if(OperatingSystem.IsLinux())
+{
+    // see: https://blog.maartenballiauw.be/post/2021/05/25/running-a-net-application-as-a-service-on-linux-with-systemd.html
+    // see: https://swimburger.net/blog/dotnet/how-to-run-a-dotnet-core-console-app-as-a-service-using-systemd-on-linux
+    // CreateApplicationBuilder would be the 'modern' way of doing things, but the UseSystemd() extension only works on IHostBuilder :(
+    var host = Host.CreateDefaultBuilder(args)  
+    .UseSystemd()
     .ConfigureServices(services =>
     {
+        services.AddSingleton<Worker>();
         services.AddHostedService<Worker>();
     })
     .Build();
+    host.Run();
+}
+else
+{
+    var builder = Host.CreateApplicationBuilder(args);
 
-host.Run();
-*/
+    builder.Services.AddSingleton<Worker>();
+    builder.Services.AddHostedService<Worker>();
+
+    var host = builder.Build();
+    host.Run();
+}
